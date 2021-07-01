d5a5eeab3847fbf51d39650a93bdf63345f51d70
Alex Sharamok <alx.sharamok@gmail.com>
2021-07-01 12:48:33 +0300

    add missing import

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,6 +1,7 @@
 import { Placeholder } from "@sitecore-jss/sitecore-jss-react";
 import styles from "./MainLayout.module.css";
 import Footer from "../Footer/Footer";
+import { getBoolEnv } from '@uniformdev/common';
 
 const esiEnabled = getBoolEnv(process.env, "UNIFORM_OPTIONS_ESI", false);
 
