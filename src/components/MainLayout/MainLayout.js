5bc602e5d93462feb3be7350528f22cf99948384
Alen Pelin <AlenPelin@users.noreply.github.com>
2022-09-26 19:30:32 +1000

    chore: use uniform v7.1.220915-1 (starter kit build 0.0.220921-1)

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,18 +1,37 @@
-import { Placeholder } from "@sitecore-jss/sitecore-jss-react";
+import React from 'react';
+import { Placeholder, useSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { useSitecoreTracker } from '@uniformdev/tracking-react';
+import { SitecorePersonalizationContextProvider } from '@uniformdev/personalize-react';
+import { EsiPlaceholder } from '@uniformdev/esi-jss-react';
+import { getNullLogger } from '@uniformdev/common';
+import { getDefaultConsoleLogger } from '@uniformdev/optimize-common-sitecore';
 import styles from "./MainLayout.module.css";
 import Footer from "../Footer/Footer";
-import { getBoolEnv } from '@uniformdev/common';
 
-const esiEnabled = getBoolEnv(process.env, "UNIFORM_OPTIONS_ESI", false);
+const enableLogging = true;
 
-const MainLayout = ({ route }) => (
-  <div className={styles.container}>
-    {esiEnabled && <Placeholder name="esi-context" rendering={route} />}
-    <main className={styles.main}>
-      <Placeholder name="uniform-jss-kit-content" rendering={route} />
-    </main>
-    <Footer />
-  </div>
-);
+export default function MainLayout({ route }) {
+  const { sitecoreContext } = useSitecoreContext()
+  const esiEnabled = route.placeholders['esi-context']?.length > 0;
+  useSitecoreTracker(sitecoreContext, {
+    type: 'jss',
+    logger: enableLogging ? getDefaultConsoleLogger("Uniform Tracker") : getNullLogger(),
+  });
+  return (
+    <SitecorePersonalizationContextProvider
+      contextData={sitecoreContext}
+      personalizationMode="jss-esi"
+      sitecoreApiKey="eefe326b-aff1-4154-9ae8-2beb85d4b8cb"
+      sitecoreSiteName="uniform-jss-kit"
+    >
+      {esiEnabled && <EsiPlaceholder rendering={route} />}
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Placeholder name="uniform-jss-kit-content" rendering={route}/>
+        </main>
+        <Footer/>
+      </div>
+    </SitecorePersonalizationContextProvider>
+  );
+};
 
-export default MainLayout;
